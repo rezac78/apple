6f0b892253f4903f87a4f6eb118777eccd363aa3
reza <69678342+rezac78@users.noreply.github.com>
2021-07-20 05:51:31 -0700

    oky

---
+++ server.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const express = require('express');
+const dotEnv = require('dotenv');
+const morgan=require('morgan');
+const connectDB=require('./config/db');
+const expressLayouts=require('express-ejs-layouts');
+
+
+dotEnv.config({ path: "./config/config.env" })
+connectDB()
+const app = express();
+
+if(process.env.NODE_ENV=="development"){
+    app.use(morgan("dev"))
+}
+app.use(expressLayouts)
+app.set("view engine", "ejs");
+app.set("layout", "./layout/MainLayout");
+app.set("views", "views");
+
+app.use(express.static(path.join(__dirname, "public")))
+
+app.use("/", require('./routers/app'))
+app.use("/users", require('./routers/users'))
+app.use("/dashboard", require('./routers/dashboard'))
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => console.log(`server running ${process.env.NODE_ENV} port ${PORT}`))
